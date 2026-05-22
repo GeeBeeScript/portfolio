e07b93e3cfc74e5c8464607572d9ab82ec0ed900
GeeBeeScript <gbemigashodimu@gmail.com>
2026-05-22 12:51:56 +0100

    nav change

--- components/sections/Navigation.tsx
+++ components/sections/Navigation.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 const Navigation = () => {
   return (
-    <nav className='my-5 w-[50%] max-sm:w-full mx-auto bg-gray-50/50 shadow-[inset_0_1px_2px_rgba(0,0,0,0.06)] text-[#444] max-w-lg flex justify-between item-center py-3 px-4 rounded-full cursor-pointer'>
+    <nav className='my-5 font-hanken max-sm:w-full mx-auto bg-gray-50/50 shadow-[inset_0_1px_2px_rgba(0,0,0,0.06)] text-[#444] max-w-lg flex justify-between item-center py-3 px-4 rounded-full cursor-pointer'>
         <div className='flex gap-2 justify-around items-center px-2 border-r-zinc-800/10 border-r'>
-            <span>Home</span> <span><House /></span>
+            <span className='sm:block hidden'>Home</span> <span><House /></span>
         </div>
         <div className='flex justify-center items-center px-3 border-r-zinc-800/10 border-r bg-blue-300'>
             <Image src="/resources/x.svg" width={10} height={10} alt='X logo' className='w-5 h-5'/>
