28050b47dc69cd08ca6e010614661de8e64f72a3
GeeBeeScript <gbemigashodimu@gmail.com>
2026-07-15 16:04:08 +0100

    little change

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       <Header />
       <Intro />
       <Stack />
-      <OfferSection />
       <PortfolioProjects />
+      <OfferSection />
       <Contact />
       <Footer />
     </main>
